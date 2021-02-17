bd75c24ea08600c14b361d025fcb49f90d0a9099
Felipe Matarazzo <felipemps@protonmail.com>
2021-02-17 16:50:17 -0300

    done\!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+puts 'creating flats...'
+
+4.times do
+  flat = Flat.create(
+    name: Faker::Lorem.sentence(word_count: 4),
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Lorem.sentence(word_count: 4, supplemental: true, random_words_to_add: 5),
+    price_per_night: rand(50..400),
+    number_of_guests: rand(2..8)
+  )
+  puts "creating flat #{flat.id}"
+end
+
+puts 'done!'
